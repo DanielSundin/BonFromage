9feb90f3b712c1262a1cf1a52373f0bd03cc763d
Daniel <79dansun@skola.boras.se>
2021-03-17 19:25:57 +0100

    models

---
+++ Models/MenuItem.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BonFromage.Models
+{
+    public class MenuItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+    }
+}
