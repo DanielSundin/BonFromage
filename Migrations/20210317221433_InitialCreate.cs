37b6ce1d9d669c2f5422e6691659989aaf9d0479
Daniel <79dansun@skola.boras.se>
2021-03-17 23:24:59 +0100

    before auth

--- Migrations/20210317221433_InitialCreate.cs
+++ Migrations/20210317221433_InitialCreate.cs
@@ -10,11 +10,11 @@ namespace BonFromage.Migrations
                 name: "MenuItem",
                 columns: table => new
                 {
-                    ID = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Price = table.Column<int>(type: "int", nullable: false)
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<int>(type: "INTEGER", nullable: false)
                 },
                 constraints: table =>
                 {
--- Models/MenuItem.cs
+++ Models/MenuItem.cs
@@ -1,14 +1,17 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BonFromage.Models
 {
     public class MenuItem
     {
         public int ID { get; set; }
+        [Display(Name = "Rätt")]
         public string Name { get; set; }
-
+        [Display(Name = "Beskrivning")]
         public string Description { get; set; }
+        [Display(Name = "Pris")]
          [DataType(DataType.Currency)]
          public int Price { get; set; }
     }
